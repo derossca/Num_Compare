5b1aef9d7eb27edd56394feb43aba545321140cf
Corey DeRossett <cderossett3@gmail.com>
2024-02-08 11:05:50 -0500

    corrected if structure, nested if statements to compare two numbers, else indicates invalid response, debugged.

--- src/Main.java
+++ src/Main.java
@@ -10,19 +10,7 @@ public class Main {
         //		num numberTwo
         //      String trash
         //		output “Enter the first number”
-        //		input numberOne
-        //		output “Enter the second number”
-        //		input numberTwo
-        //		if numberOne == numberTwo then
-        //			output numberOne + “ is equal to “ + numberTwo
-        //		else  if numberOne > numberTwo then
-        //			output numberTwo + “ is less than “ + numberOne
-        //		else if numberOne < numberTwo then
-        //			output numberOne + “ is less than “ + numberTwo
-        //      else
-        //          output "You said the number is " + trash
-        //          output "Please run the program again and enter a valid number!"
-        //		end if
+        //
         //	return
         //end class
 
@@ -32,5 +20,44 @@ public class Main {
         double numberTwo = 0;
         String trash = ""; //using this string in case user enters a non numeric value
 
+        //output asking for first number from user
+        System.out.println("Enter the first number:");
+
+        //using if else structure so that if a valid number is inputted then program will proceed but if not then
+        // program indicates invalid number
+        if(in.hasNextDouble())
+        {
+            numberOne = in.nextDouble();
+            in.nextLine(); //clearing buffer
+            System.out.println("Enter the second number: ");
+            if(in.hasNextDouble()) //nesting if statement here to ask user for second number to then compare
+            {
+                numberTwo = in.nextDouble();
+                in.nextLine();
+                if(numberOne == numberTwo) //nesting if statement here to run program and compare the numbers indicating if they are equal or the lesser oen
+                {
+                    System.out.println(numberOne + " is equal to " + numberTwo);
+                }
+                else if(numberOne > numberTwo)
+                {
+                    System.out.println(numberTwo + " is less than " + numberOne);
+                }
+                else {
+                    System.out.println(numberOne + " is less than " + numberTwo);
+                }
+            }
+            else
+            {
+                trash = in.nextLine();
+                System.out.println("You said the number is " + trash);
+                System.out.println("Please run the program again and enter a valid number!");
+            }
+        }
+        else
+        {
+            trash = in.nextLine(); //because the input is not a valid number so it is read as a string
+            System.out.println("You said the number is " + trash);
+            System.out.println("Please run the program again and enter a valid number!");
+        }
     }
 }
