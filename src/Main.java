caadda2b5a26e1fc58286806cb1c8810ffb4b307
Corey DeRossett <cderossett3@gmail.com>
2024-02-08 10:42:31 -0500

    removed if structure to restart and debug

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+//importing scanner
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        //class
+        //	main()
+        //		num numberOne
+        //		num numberTwo
+        //      String trash
+        //		output “Enter the first number”
+        //		input numberOne
+        //		output “Enter the second number”
+        //		input numberTwo
+        //		if numberOne == numberTwo then
+        //			output numberOne + “ is equal to “ + numberTwo
+        //		else  if numberOne > numberTwo then
+        //			output numberTwo + “ is less than “ + numberOne
+        //		else if numberOne < numberTwo then
+        //			output numberOne + “ is less than “ + numberTwo
+        //      else
+        //          output "You said the number is " + trash
+        //          output "Please run the program again and enter a valid number!"
+        //		end if
+        //	return
+        //end class
+
+        //declaring variables
+        Scanner in = new Scanner(System.in);
+        double numberOne = 0; //using doubles in case user enters decimal
+        double numberTwo = 0;
+        String trash = ""; //using this string in case user enters a non numeric value
+
     }
 }
